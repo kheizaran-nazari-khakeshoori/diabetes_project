ac085405472167182351108f055a454b1b78d250
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-07-13 19:01:55 +0200

    Fix main pipeline entry point

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from src.data_cleaning import clean_data, load_data, save_cleaned_data
 from src.feature_scaling import scale_features
+from src.feature_selection import select_features
 
 
 def main():
@@ -19,6 +20,11 @@ def main():
     
     # ۳. مرحله دوم: مقیاس‌دهی ویژگی‌ها (Feature Scaling)
     scale_features(CLEANED_DATA_PATH, PROCESSED_DIR_PATH)
+    print("-" * 40)
+    
+    # ۴. مرحله سوم: تحلیل و انتخاب ویژگی‌ها (Feature Selection)
+    select_features(CLEANED_DATA_PATH)
+    
     print("\n🚀 --- Pipeline Executed Successfully! --- 🚀")
 
 if __name__ == "__main__":
